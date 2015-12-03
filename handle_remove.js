655ecfc5110968394ca6fd2713379ad66697872f
dmoon <dmoon.t@gmail.com>
2015-12-04 00:13:26 +0800

    remove something useless

--- handle_remove.js
+++ handle_remove.js
@@ -1,25 +1,12 @@
-this.cookies_ = {};
-
-function addCookie(cookie) {
-  var key = cookie.name + cookie.domain + cookie.hostOnly + cookie.path + cookie.secure + cookie.httpOnly + cookie.session + cookie.storeId;
-  // console.log(key);
-  this.cookies_[key] = cookie;
-}
-
-function listener(info) {
-  var cookie = info.cookie;
-  addCookie(cookie);
-}
+// this.cookies_ = {};
 
 function clearCookiesByDomain(domainName) {
   if (domainName.length > 0) {
     chrome.cookies.getAll({
       domain: domainName
     }, function(cookies) {
-      console.log(JSON.stringify(cookies,null,4));
       for (var i = 0; i < cookies.length; i++) {
         var url = "http" + (cookies[i].secure ? "s" : "") + "://" + domainName + cookies[i].path;
-        console.log(url);
         chrome.cookies.remove({
           url: url,
           name: cookies[i].name
@@ -28,19 +15,18 @@ function clearCookiesByDomain(domainName) {
     });
   }
   else
-    alert('Give me a domainName');
+    console.log('Give me a domainName');
 }
 
 jQuery(document).ready(function() {
-  console.log("From cookie_handler.js:");
   if (!chrome.cookies) {
     chrome.cookies = chrome.experimental.cookies;
   }
   chrome.browserAction.onClicked.addListener(function(tab) {
     var url = purl(tab.url);
     var domainName = url.attr('host');
-    console.log(url);
-    console.log(domainName);
+    console.log('url: '+ url);
+    console.log('domainName: '+ domainName);
     clearCookiesByDomain(domainName);
   });
 });
